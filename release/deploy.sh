1e31bd9f2b5c55bad97fb73fb5dff25657a2206a
Richard North <rich.north@gmail.com>
2017-11-12 13:58:48 +0000

    Revert pwd for deployment script execution

--- release/deploy.sh
+++ release/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 openssl aes-256-cbc -K $encrypted_99aebaea6bf8_key -iv $encrypted_99aebaea6bf8_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
 gpg --fast-import release/codesigning.asc
 
